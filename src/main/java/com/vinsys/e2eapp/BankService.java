b913434e530fabc1fb7f1256cc0b69d50446b909
Siddhartha <sidmotghare91@gmail.com>
2018-10-09 15:02:58 +0530

    feat(api to update): Api to update account details
    fix(api map): Duplicate method name in controller
    
    fix(imports): Add missing imports
    
    fix(sonar): Sonar issues fix.

--- src/main/java/com/vinsys/e2eapp/BankService.java
+++ src/main/java/com/vinsys/e2eapp/BankService.java
@@ -2,18 +2,23 @@ package com.vinsys.e2eapp;
 
 import java.util.logging.Logger;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BankService {
-	Logger logger = Logger.getLogger("");
+	Logger logger = Logger.getLogger("BankService");
 	// we know that service
-	@RequestMapping(method = RequestMethod.GET, path = "account")
+	@GetMapping(path = "/account")
 	public String openAccount(@RequestParam(name = "accountid") String accountid) {
-		logger.info("you are opening a account with " + accountid);
+		logger.info(String.format("you are opening a account with %s ", accountid));
 		return "You Opened a Acccount Successfully";
+	}
+
+	@GetMapping(path = "/accountupdate")
+	public String updateAccount(@RequestParam(name = "accountid") String accountid) {
+		logger.info(String.format("you are details are updated for account with %s", accountid));
+		return "You Updated a Acccount Successfully";
 	}
 }
