674335558f072411a84104e1c793f2e3545551ac
nilesh.devdas@gmail.com <nilesh.devdas@gmail.com>
2018-10-09 17:06:06 +0530

    Plugin update

--- src/main/java/com/vinsys/e2eapp/BankService.java
+++ src/main/java/com/vinsys/e2eapp/BankService.java
@@ -10,10 +10,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BankService {
 	Logger logger = Logger.getLogger("");
+
 	// we know that service
 	@RequestMapping(method = RequestMethod.GET, path = "account")
 	public String openAccount(@RequestParam(name = "accountid") String accountid) {
 		logger.info("you are opening a account with " + accountid);
 		return "You Opened a Acccount Successfully";
 	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "account1")
+	public String openAccount1(@RequestParam(name = "accountid") String accountid) {
+		logger.info("you are opening a account with " + accountid);
+		return "You Opened a Acccount Successfully";
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "account2")
+	public String openAccount2(@RequestParam(name = "accountid2") String accountid) {
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+		logger.info("you are opening a account with " + accountid);
+
+		return "You Opened a Acccount Successfully";
+	}
+
 }
